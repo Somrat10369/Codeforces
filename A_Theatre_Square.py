77fafeeca7cb44ca802024739809aa406205c491
Nur Nobin <somrat10369@gmail.com>
2026-06-02 15:18:18 +0600

    Add solutions for A_Domino_piling, A_Next_Round, and A_Theatre_Square problems

---
+++ A_Theatre_Square.py
@@ -0,0 +1,25 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+mint = lambda: map(int, input().split())
+lint = lambda: list(map(int, input().split()))
+
+def solve():
+    A,B,C = mint()
+    ans = -(-A//C) * -(-B//C)
+    print(ans)
+
+def main():
+    sys.setrecursionlimit(200000)
+
+    t = 1
+    #try:
+        #t = int(input())
+    #except ValueError:
+        #pass
+
+    for _ in range(t):
+        solve()
+
+if __name__ == '__main__':
+    main()
