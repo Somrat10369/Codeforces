204c51f5648c0719ccd81e52396851d3189c39f9
Nur Nobin <somrat10369@gmail.com>
2026-06-03 21:37:07 +0600

    Add solutions for A_Bear_and_Big_Brother and A_Elephant problems

---
+++ A_Bear_and_Big_Brother.py
@@ -0,0 +1,28 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+mint = lambda: map(int, input().split())
+lint = lambda: list(map(int, input().split()))
+
+def solve():
+    A,B = mint()
+    count = 0
+    while A<=B:
+      A, B = 3*A , 2*B
+      count+=1
+    print(count)
+
+def main():
+    sys.setrecursionlimit(200000)
+
+    t = 1
+    #try:
+        #t = int(input())
+    #except ValueError:
+        #pass
+
+    for _ in range(t):
+        solve()
+
+if __name__ == '__main__':
+    main()
