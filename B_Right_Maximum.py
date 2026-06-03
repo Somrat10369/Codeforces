19b466595fcd6988924d711151eaff45da01f9c2
Nur Nobin <somrat10369@gmail.com>
2026-06-04 00:07:11 +0600

    Refactor B_Right_Maximum solution to simplify maximum counting logic

--- B_Right_Maximum.py
+++ B_Right_Maximum.py
@@ -6,17 +6,16 @@ lint = lambda: list(map(int, input().split()))
 def solve():
     n = int(input())
     arr = lint()
-
     count = 0
-    current_max = -1
+    cmx = -9999999999
 
-    for i in range(n - 1, -1, -1):
-        if arr[i] >= current_max:
-            current_max = arr[i]
-            count += 1
+    for i in arr:
+      if i >= cmx:
+        cmx = i
+        count+=1
 
     print(count)
-
+    
 def main():
     t = 1
     try:
