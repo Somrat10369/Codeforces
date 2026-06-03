b9c49a0c3e9f287b57a90f912b03ec7418b060ce
Nur Nobin <somrat10369@gmail.com>
2026-06-03 22:07:34 +0600

    Add solution for A_Soldier_and_Bananas problem

---
+++ A_Soldier_and_Bananas.py
@@ -0,0 +1,25 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+mint = lambda: map(int, input().split())
+lint = lambda: list(map(int, input().split()))
+
+def solve():
+    k,n,w = mint()
+    sum_price = w*(w+1)*k//2 - n
+    print(sum_price) if sum_price > 0 else print(0)
+
+def main():
+    sys.setrecursionlimit(200000)
+
+    t = 1
+    #try:
+        #t = int(input())
+    #except ValueError:
+        #pass
+
+    for _ in range(t):
+        solve()
+
+if __name__ == '__main__':
+    main()
