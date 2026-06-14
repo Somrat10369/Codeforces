ac91bbc7e1c7c51b6bfbaaf9bd8b807e3759a73f
Nur Nobin <somrat10369@gmail.com>
2026-06-14 16:37:26 +0600

    Another one

---
+++ A_Nearly_Lucky_Number.py
@@ -0,0 +1,26 @@
+import sys
+from collections import Counter
+
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+mint = lambda: map(int, input().split())
+lint = lambda: list(map(int, input().split()))
+
+def solve():
+    dic = Counter(input())
+    num = (dic["4"] + dic["7"])
+    print('YES') if num == 4 or num == 7 else print('NO')
+
+def main():
+    sys.setrecursionlimit(200000)
+
+    t = 1
+    #try:
+        #t = int(input())
+    #except ValueError:
+        #pass
+
+    for _ in range(t):
+        solve()
+
+if __name__ == '__main__':
+    main()
