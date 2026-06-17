07c2240955e756fec5ca1bb4d2c8405cd7850f4c
Nur Nobin <somrat10369@gmail.com>
2026-06-17 08:06:24 +0600

    some more problems

---
+++ New_Year_Resolution.py
@@ -0,0 +1,24 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+mint = lambda: map(int, input().split())
+lint = lambda: list(map(int, input().split()))
+
+def solve():
+    n = int(input())
+    print(31*n)
+
+def main():
+    sys.setrecursionlimit(200000)
+
+    t = 1
+    #try:
+        #t = int(input())
+    #except ValueError:
+        #pass
+
+    for _ in range(t):
+        solve()
+
+if __name__ == '__main__':
+    main()
